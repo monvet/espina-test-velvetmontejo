15d14fdd4fa1f6c49107565b039ee11c4ad3cd67
Velvet Montejo <vmontejo.imf@gmail.com>
2025-08-20 10:26:57 -0600

    fix: duration and style of js spin animation

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+// Log en consola al hacer click en el icono (solo un pequeño guiño, literalmente)
 const smiley = document.querySelector(".smiley-icon");
-smiley.addEventListener("mouseenter", ()=>{
+smiley.addEventListener("click", ()=>{
     console.log("Hola;)");
 });
 
+// Animación de highlight al hacer hover en las tarjetas
 document.querySelectorAll('.card').forEach(card => {
     card.addEventListener('mouseenter', () => card.classList.add('highlight'));
     card.addEventListener('mouseleave', () => card.classList.remove('highlight'));
 })
+
+// Animación de giro al hacer click en las tarjetas
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", () => {
+        card.classList.add("spin");
+
+    // Quita la clase para poder repetir la animacion
+        card.addEventListener("animationend", () => {
+        card.classList.remove("spin");
+        }, { once: true });
+    });
+});
